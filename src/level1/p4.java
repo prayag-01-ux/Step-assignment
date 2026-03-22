033232ebbf7186cba6f94f0e0e838a000f19b3c3
prayag-01-ux <prayagdash797856@gmail.com>
2026-03-22 18:00:12 +0530

    level1

---
+++ src/level1/p4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class p4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] arr = new double[10];
+        double total = 0.0;
+        int index = 0;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            double num = sc.nextDouble();
+
+            if (num <= 0 || index == 10) {
+                break;
+            }
+
+            arr[index] = num;
+            index++;
+        }
+
+        // Display numbers and sum
+        System.out.println("Entered numbers:");
+        for (int i = 0; i < index; i++) {
+            System.out.println(arr[i]);
+            total += arr[i];
+        }
+
+        System.out.println("Total sum = " + total);
+    }
+}
