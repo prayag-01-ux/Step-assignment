e84d0ced1e352045b5ca29c8421163819d53d0c0
prayag-01-ux <prayagdash797856@gmail.com>
2026-03-25 14:05:52 +0530

    level3

---
+++ src/level3/p1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year should be >= 1582");
+        } else {
+            if (year % 400 == 0) {
+                System.out.println("Year is a Leap Year");
+            } else if (year % 100 == 0) {
+                System.out.println("Year is not a Leap Year");
+            } else if (year % 4 == 0) {
+                System.out.println("Year is a Leap Year");
+            } else {
+                System.out.println("Year is not a Leap Year");
+            }
+        }
+    }
+}
